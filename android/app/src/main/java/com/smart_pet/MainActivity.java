f96d258cf3ad4301c4f61446233ca60632cfdbb7
casio2978 <casio2978@naver.com>
2022-11-18 16:50:58 +0900

    [ADD] bottom nav (22.11.18)  - 이미지 아이콘 및 프래그먼트  - MainActivity 프래그먼트 전환 코드

--- android/app/src/main/java/com/smart_pet/MainActivity.java
+++ android/app/src/main/java/com/smart_pet/MainActivity.java
@@ -1,14 +1,30 @@
 package com.smart_pet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity{
+
+    private NavController nav_host;
+    private BottomNavigationView nav_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bottom nav bar
+        nav_host = Navigation.findNavController(this, R.id.nav_host);
+        nav_bar = findViewById(R.id.nav_bar);
+        NavigationUI.setupWithNavController(nav_bar, nav_host);
     }
+
+
 }

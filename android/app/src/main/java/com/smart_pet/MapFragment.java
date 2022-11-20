aa9dd196f2c09bb3fc34ca7a4a2da308b3405f40
sey2 <casio2978@naver.com>
2022-11-21 02:22:07 +0900

    [ADD] CSV File Reader (22.11.21)

--- android/app/src/main/java/com/smart_pet/MapFragment.java
+++ android/app/src/main/java/com/smart_pet/MapFragment.java
@@ -1,13 +1,26 @@
 package com.smart_pet;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
 import net.daum.mf.map.api.MapView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MapFragment extends Fragment {
@@ -21,7 +34,26 @@ public class MapFragment extends Fragment {
 
         ViewGroup mapViewContainer = (ViewGroup) rootView.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+        try {
+            loadData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvException e) {
+            e.printStackTrace();
+        }
 
         return rootView;
+    }
+
+    private void loadData() throws IOException, CsvException {
+        AssetManager assetManager = this.getActivity().getAssets();
+        InputStream inputStream = assetManager.open("hospital.csv");
+        CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream, "EUC-KR"));
+
+        List<String[]> allContent = (List<String[]>) csvReader.readAll();
+        for(String content[] : allContent){
+            StringBuilder sb = new StringBuilder("");
+            Log.d("csv", content[19] + " 병원명: " + content[21] + " X: " + content[26] + " Y: " + content[27] );
+        }
     }
 }
